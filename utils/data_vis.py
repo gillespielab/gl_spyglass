5f7312c94d882365a99983c008421e2f94ab4bfc
Molly Park <mollysypark@gmail.com>
2024-10-16 20:41:03 -0700

    add correct goal fraction calculations for trials_since_found offsets

--- utils/data_vis.py
+++ utils/data_vis.py
@@ -3,66 +3,61 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 def get_goal_fractions(df):
-    
-    num_trials = len(df)
-    goals = df["goal_well"].to_numpy()
+    # Cut off any incomplete trial data from the end
+    filtered_df = df.query("goal_well > 0")
+    num_trials = len(filtered_df)
+    goals = filtered_df["goal_well"].to_numpy()
+
+    # Get indices of trials that were the 1st in their block
     goal_change_trials = np.where(np.diff(goals, prepend=0, append=num_trials) != 0)[0]
-
-    # boolean mask: is_goal[i] = 1 iff goal arm was visited on trial i
-    goal_trials = df.index[df.outer_well == df.goal_well].to_numpy()
-
+    # Get indices of goal trials
+    goal_trials = filtered_df.index[filtered_df.outer_well == filtered_df.goal_well].to_numpy()
     is_goal = np.zeros(num_trials, dtype=int)
     is_goal[goal_trials] = 1
 
-    # map each trial to the number of trials that have passed since last goal-change / reward
-    since_goal_change = np.zeros_like(goals)
-    since_goal_found = np.zeros_like(goals)
-    since_goal_found.fill(-1)
-    first_goals = []
-    for i in range(1, goal_change_trials.size):
-        block_start = goal_change_trials[i-1]
-        block_end = goal_change_trials[i]
-
-        slice = is_goal[block_start:block_end]
-        first_goal = np.argmax(slice) + block_start
-        if first_goal != block_start: # argmax = 0 if there were no goals
-            first_goals.append(first_goal)
-        
-        counts_since_change = np.arange(1, block_end - block_start + 1)
-        counts_since_found = counts_since_change - (first_goal - block_start) # align to first goal trial of block
-        since_goal_change[block_start:block_end] = counts_since_change
-        since_goal_found[block_start:block_end] = counts_since_found
-
-    # calculating goal fractions wrt trials since goal-change / reward
-    cumulative_goals = np.cumsum(is_goal) # cg[i] = cumulative # of goals up to and including trial i
-    # cg[j] - cg[i] = cumulative goals at trial j made since trial i 
-
+    # Map each trial to the number of trials that have passed since:
+    # - the 1st trial of the current block
+    # - the 1st rewarded trial of the current block
+    since_goal_change = np.zeros(num_trials, dtype=np.int64)
+    since_goal_found = np.zeros(num_trials, dtype=np.int64)
     goal_fraction_since_change = np.zeros(num_trials)
-    goal_fraction_since_found = np.zeros(num_trials)
-    goal_fraction_since_found.fill(-1)
 
+    # Use prefix sum to calculate cumulative goals since the 1st trial/goal of each block,
+    # i.e. cg[i] = num goals accumulated from trial 0 to i (inclusive)
+    cumulative_goals = np.cumsum(is_goal)
+
+    # Calculating goal fraction history for each block
     for i in range(1, goal_change_trials.size):
         block_start = goal_change_trials[i-1]
         block_end = goal_change_trials[i]
-        if i == 1: # fenceposting
-            goal_fraction_since_change[block_start:block_end] = cumulative_goals[block_start:block_end] / since_goal_change[block_start:block_end]
-        else:
-            goal_fraction_since_change[block_start:block_end] = (cumulative_goals[block_start:block_end] - cumulative_goals[block_start-1]) / since_goal_change[block_start:block_end]
+        block_len = block_end - block_start
+        # Get index of first goal trial within the block
+        slice = is_goal[block_start:block_end]
+        if not np.any(slice):
+            # We only consider complete trials, so we shouldn't hit this case
+            raise Exception("Invalid input: found incomplete trial")
+        first_goal = np.argmax(slice) + block_start
+        
+        counts_since_change = np.arange(block_len)
+        # pre-goal trials will have a negative count_since_found value
+        counts_since_found = counts_since_change - (first_goal - block_start)
+        since_goal_change[block_start:block_end] = counts_since_change + 1
+        since_goal_found[block_start:block_end] = counts_since_found + 1
 
-        first_goal = goal_change_trials[i-1]
-        while first_goal < block_end and not is_goal[first_goal]:
-            first_goal += 1 # increment until we find first goal trial within block
-        if first_goal < block_end:
-            goal_fraction_since_found[first_goal:block_end] = (cumulative_goals[first_goal:block_end] - cumulative_goals[first_goal-1]) / since_goal_change[first_goal:block_end]
+        # Get array of accumulated goals at each trial in the current block
+        prev_goals = 0 if block_start == 0 else cumulative_goals[block_start-1]
+        curr_block_goal_hist = cumulative_goals[block_start:block_end] - prev_goals
+        trials_since_change = since_goal_change[block_start:block_end]
 
-    # handle divide by 0 instances on 1st trials of each block and reward trials
+        goal_fraction_since_change[block_start:block_end] = \
+            curr_block_goal_hist / trials_since_change
+
+    # handle divide by 0 instances (i.e. trial before a goal)
     goal_fraction_since_change = np.nan_to_num(goal_fraction_since_change)
-    goal_fraction_since_found = np.nan_to_num(goal_fraction_since_found)
 
     data = {}
     data["goal_fraction_since_change"] = goal_fraction_since_change
     data["trials_since_change"] = since_goal_change
-    data["goal_fraction_since_found"] = goal_fraction_since_found
     data["trials_since_found"] = since_goal_found
     return data
 
