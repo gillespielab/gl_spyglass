397b8c4f07244cba8eb699e94fbff0870a519e95
sophie-robertson <slr1234@uw.edu>
2025-07-23 11:04:16 -0700

    updates to trial info 8 arm and trial info parser to fix some bugs and add some extra informative columns in the trials_df

--- trial_info.py
+++ trial_info.py
@@ -147,47 +147,6 @@ def get_beam_times(key, nwbf, beam):
     start_time, end_time = epoch_valid_times.squeeze()
     return diotimesall[(diotimesall >= start_time) & (diotimesall < end_time)]
 
-def get_dio_event_times(key, nwbf, dio_event_name):
-
-    dio_obj_id = (sgc.DIOEvents & {'nwb_file_name': key['nwb_file_name'], 'dio_event_name': dio_event_name}).fetch1('dio_object_id')
-    dios = nwbf.objects[dio_obj_id]
-    dio_times_all = np.asarray(dios.timestamps)
-    dio_data_all = np.asarray(dios.data)
-
-    # get start and end times of the epoch
-    epoch_name = (sgc.TaskEpoch & {"nwb_file_name": key["nwb_file_name"], "epoch": key["epoch"]}).fetch1("interval_list_name")
-    epoch_valid_times = ( # gets time bounds of epoch
-        sgc.IntervalList & {"nwb_file_name" : key["nwb_file_name"], "interval_list_name": epoch_name}
-    ).fetch1("valid_times")
-    epoch_start_time, epoch_end_time = epoch_valid_times.squeeze()
-
-    # constrain the extracted data to times within this epoch
-    epoch_time_mask = (dio_times_all >= epoch_start_time) & (dio_times_all < epoch_end_time)
-    dio_times = dio_times_all[epoch_time_mask]
-    dio_data = dio_data_all[epoch_time_mask]
-
-    # get start and end indices of the dio event being on
-    n_dio_events = len(np.where(dio_data == 1)[0])
-    dio_events_start_idx = np.where(dio_data == 1)[0].astype('int')
-    dio_events_end_idx = []
-    for start in dio_events_start_idx:
-        end = np.where(dio_data[start + 1:] == 0)[0]
-        if end.size > 0:
-            dio_events_end_idx.append(start + 1 + end[0])
-
-    # get start and end times of dio event being on
-    dio_events_start_times = dio_times[dio_events_start_idx]
-    dio_events_end_times = dio_times[dio_events_end_idx]
-
-    # pad end times with end time of the epoch data to account for the case where the homelight was left on after the run session
-    len_diff = len(dio_events_start_times) - len(dio_events_end_times)
-    for i in range(len_diff):
-        dio_events_end_times = np.append(dio_events_end_times, epoch_end_time)
-
-    dio_events_intervals = np.vstack([dio_events_start_times, dio_events_end_times]).T
-
-    return dio_events_start_times, dio_events_end_times, dio_events_intervals
-
 def get_dio_mapping(key, nwbf):
     # get dio mapping for arms
     diomap = {}
@@ -199,7 +158,7 @@ def get_dio_mapping(key, nwbf):
         diomap[dio_obj.name] = chan_num
     return diomap
 
-def get_sc_descriptors(sc_text):
+def get_sc_descriptors(sc_text, nwb_file_name):
     """
     Helper method to retrieve key descriptors from the statescript log. 
 
@@ -223,13 +182,37 @@ def get_sc_descriptors(sc_text):
             descriptors["python_script"] = line[1:-1]
         elif re.match(r"^(int lockoutPeriod\s*=?).*", line): # get lockout period length (in seconds)
             descriptors["lockout_period"] = int(line[line.index("=") + 1 : ].strip()) / 1000
-        elif re.match(r"^(outerReps\s*=?).*", line):
-            if "np.random.randint" in line:
-                rangestr = line[line.index("(") + 1 : line.index(")")]
-                range = rangestr.split(",")
-                descriptors["outer_reps"] = [int(range[0]), int(range[1])]
+        
+        elif 'variable_outer_reps' in line:
+            print('found!')
+            print(line)
+            line_parts = line.split(" ")
+            if line_parts[0] == 'variable_outer_reps':
+                print(line_parts)
+                if line_parts[2] == 'True':
+                    variable_outer_reps = True
+        
+        elif 'outerReps = ' in line:
+            if ('timothy' in nwb_file_name) | ('tony' in nwb_file_name) | ('teddy' in nwb_file_name):
+                line_parts = line.split(" ")
+                # variable outer reps set as True 
+                if variable_outer_reps & ('np.random.randint' in line):
+                    rangestr = line[line.index("(") + 1 : line.index(")")]
+                    range = rangestr.split(",")
+                    descriptors['outer_reps'] = [int(range[0]), int(range[1])]
+                elif ~variable_outer_reps & ('np.random.randint' not in line):
+                    descriptors['outer_reps'] = int(line[line.index("=") + 1:].strip())
+                else:
+                    continue
             else:
-                descriptors["outer_reps"] = int(line[line.index("=") + 1:].strip())
+                if re.match(r"^(outerReps\s*=?).*", line):
+                    if "np.random.randint" in line:
+                        rangestr = line[line.index("(") + 1 : line.index(")")]
+                        range = rangestr.split(",")
+                        descriptors["outer_reps"] = [int(range[0]), int(range[1])]
+                    else:
+                        descriptors["outer_reps"] = int(line[line.index("=") + 1:].strip())
+
         elif re.match(r"^(numgoals\s*=?).*", line):
             descriptors["num_goals"] = int(line[line.index("=") + 1:].strip())
         elif re.match(r"^(forageNum\s*=?).*", line):
@@ -276,8 +259,9 @@ class TrialInfo8Arm(SpyglassMixin, dj.Computed):
     def make(self, key):
         """
         Parses the given StateScriptFile into landmark behavioral events
-        and saves them as an NWB analysis file.
+        and saves them as an NWB analysis file containing a trial info dataframe.
         """
+
         # ignore all DIO events that aren't homebeam to avoid repeats
         if key["dio_event_name"] != "homebeam":
             return
@@ -289,8 +273,8 @@ class TrialInfo8Arm(SpyglassMixin, dj.Computed):
         # get first home poke time of the epoch for calculating trodes to ptp time offset
         nwb_file_name = key["nwb_file_name"]
         epoch_num = key["epoch"]
-        home_times = get_beam_times(key, nwbf, "homebeam")
-        if home_times.size == 0:
+        home_on_dio_times, _, _ = get_dio_event_times(key, nwbf, "homebeam")
+        if home_on_dio_times.size == 0:
             logger.info(f"Skipping epoch: No home dio events detected for {nwb_file_name}, epoch {epoch_num}")
             return
         
@@ -300,13 +284,13 @@ class TrialInfo8Arm(SpyglassMixin, dj.Computed):
         # get statescriptlog contents and get descriptors
         file_id = (StateScriptFile & key).fetch1("file_object_id")
         sc = nwbf.objects[file_id]
-        key["descriptors"] = get_sc_descriptors(sc.content)
+        key["descriptors"] = get_sc_descriptors(sc.content, nwb_file_name)
 
-        # parse statescript log according to the task type (currently there's only the V8, but future variants will be added here)
+        # parse statescript log according to the task name
         task_name = (sgc.TaskEpoch & {"nwb_file_name": key["nwb_file_name"], "epoch": key["epoch"]}).fetch1("task_name")
         if task_name == "Eight arm flexible spatial task":
             key["parser"] = "V8_delay"
-            parser = V8TrialParser(sc.content, dio_map, home_times, key, new=True)
+            parser = V8TrialParser(sc.content, dio_map, home_on_dio_times, key, nwbf)
         elif task_name == "Sleep": # just in case
             logger.info(f"Skipping sleep epoch: {nwb_file_name}, epoch {epoch_num}")
             return 
@@ -381,23 +365,26 @@ class TrialInfo8Arm(SpyglassMixin, dj.Computed):
             print(f"No parsing logic implemented for task: {task_name}")
 
 def get_beam_times(key, nwbf, beam):
+    # potentially outdated function, use get_dio_event_times() which is more general and returns more relevant outputs
+    interval_list_name = (sgc.TaskEpoch & {"nwb_file_name": key["nwb_file_name"], "epoch": key["epoch"]}).fetch1("interval_list_name")
+
     # get homebeam dio event timestamps
     dio_obj_id = (
-        sgc.DIOEvents & {"nwb_file_name": key["nwb_file_name"], "dio_event_name": beam}
+        sgc.DIOEvents & {"nwb_file_name": key["nwb_file_name"], "dio_event_name": beam, "interval_list_name": interval_list_name}
     ).fetch1("dio_object_id")
     dios = nwbf.objects[dio_obj_id]
     diotimesall = np.asarray(dios.timestamps)
 
     # get timestamp of 1st homewell trigger the given epoch to calculate time offset
-    epoch_name = (sgc.TaskEpoch & {"nwb_file_name": key["nwb_file_name"], "epoch": key["epoch"]}).fetch1("interval_list_name")
     epoch_valid_times = ( # gets time bounds of epoch
-        sgc.IntervalList & {"nwb_file_name" : key["nwb_file_name"], "interval_list_name": epoch_name}
+        sgc.IntervalList & {"nwb_file_name" : key["nwb_file_name"], "interval_list_name": interval_list_name}
     ).fetch1("valid_times")
     start_time, end_time = epoch_valid_times.squeeze()
+
     return diotimesall[(diotimesall >= start_time) & (diotimesall < end_time)]
 
 def get_dio_mapping(key, nwbf):
-    # get dio mapping for arms
+    # get mapping between the dio numbers and the actual arm numbers for the 1-8 arms
     diomap = {}
     dio_obj_ids = (DIOEvents & {"nwb_file_name": key["nwb_file_name"]}).fetch("dio_object_id")
     for id in dio_obj_ids:
@@ -407,7 +394,7 @@ def get_dio_mapping(key, nwbf):
         diomap[dio_obj.name] = chan_num
     return diomap
 
-def get_sc_descriptors(sc_text):
+def get_sc_descriptors(sc_text, nwb_file_name):
     """
     Helper method to retrieve key descriptors from the statescript log. 
 
@@ -418,6 +405,8 @@ def get_sc_descriptors(sc_text):
     sc_lines = sc_text.split("\n")
 
     lines = [line[1:] for line in sc_lines if len(line) > 0 and line[0]=="#"]
+
+    variable_outer_reps = False
 
     for line in lines:
         if "%" in line:
@@ -431,13 +420,34 @@ def get_sc_descriptors(sc_text):
             descriptors["python_script"] = line[1:-1]
         elif re.match(r"^(int lockoutPeriod\s*=?).*", line): # get lockout period length (in seconds)
             descriptors["lockout_period"] = int(line[line.index("=") + 1 : ].strip()) / 1000
-        elif re.match(r"^(outerReps\s*=?).*", line):
-            if "np.random.randint" in line:
-                rangestr = line[line.index("(") + 1 : line.index(")")]
-                range = rangestr.split(",")
-                descriptors["outer_reps"] = [int(range[0]), int(range[1])]
+        
+        elif 'variable_outer_reps' in line:
+            line_parts = line.split(" ")
+            if line_parts[0] == 'variable_outer_reps':
+                if line_parts[2] == 'True':
+                    variable_outer_reps = True
+        
+        elif 'outerReps = ' in line:
+            if ('timothy' in nwb_file_name) | ('tony' in nwb_file_name) | ('teddy' in nwb_file_name):
+                line_parts = line.split(" ")
+                # variable outer reps set as True 
+                if variable_outer_reps & ('np.random.randint' in line):
+                    rangestr = line[line.index("(") + 1 : line.index(")")]
+                    range = rangestr.split(",")
+                    descriptors['outer_reps'] = [int(range[0]), int(range[1])]
+                elif ~variable_outer_reps & ('np.random.randint' not in line):
+                    descriptors['outer_reps'] = int(line[line.index("=") + 1:].strip())
+                else:
+                    continue
             else:
-                descriptors["outer_reps"] = int(line[line.index("=") + 1:].strip())
+                if re.match(r"^(outerReps\s*=?).*", line):
+                    if "np.random.randint" in line:
+                        rangestr = line[line.index("(") + 1 : line.index(")")]
+                        range = rangestr.split(",")
+                        descriptors["outer_reps"] = [int(range[0]), int(range[1])]
+                    else:
+                        descriptors["outer_reps"] = int(line[line.index("=") + 1:].strip())
+
         elif re.match(r"^(numgoals\s*=?).*", line):
             descriptors["num_goals"] = int(line[line.index("=") + 1:].strip())
         elif re.match(r"^(forageNum\s*=?).*", line):
@@ -447,3 +457,45 @@ def get_sc_descriptors(sc_text):
     #     raise Exception("No lockout period found in StateScriptLog")
 
     return descriptors
+
+def get_dio_event_times(key, nwbf, dio_event_name):
+    # helper function to retrieve relevant dio times for any dio event name (sorts into start times, end times, and combined intervals)
+
+    dio_obj_id = (sgc.DIOEvents & {'nwb_file_name': key['nwb_file_name'], 'dio_event_name': dio_event_name, 'interval_list_name': "raw data valid times"}).fetch1('dio_object_id')
+    dios = nwbf.objects[dio_obj_id]
+    dio_times_all = np.asarray(dios.timestamps)
+    dio_data_all = np.asarray(dios.data)
+
+    # get start and end times of the epoch
+    epoch_name = (sgc.TaskEpoch & {"nwb_file_name": key["nwb_file_name"], "epoch": key["epoch"]}).fetch1("interval_list_name")
+    epoch_valid_times = ( # gets time bounds of epoch
+        sgc.IntervalList & {"nwb_file_name" : key["nwb_file_name"], "interval_list_name": epoch_name}
+    ).fetch1("valid_times")
+    epoch_start_time, epoch_end_time = epoch_valid_times.squeeze()
+
+    # constrain the extracted data to times within this epoch
+    epoch_time_mask = (dio_times_all >= epoch_start_time) & (dio_times_all < epoch_end_time)
+    dio_times = dio_times_all[epoch_time_mask]
+    dio_data = dio_data_all[epoch_time_mask]
+
+    # get start and end indices of the dio event being on
+    n_dio_events = len(np.where(dio_data == 1)[0])
+    dio_events_start_idx = np.where(dio_data == 1)[0].astype('int')
+    dio_events_end_idx = []
+    for start in dio_events_start_idx:
+        end = np.where(dio_data[start + 1:] == 0)[0]
+        if end.size > 0:
+            dio_events_end_idx.append(start + 1 + end[0])
+
+    # get start and end times of dio event being on
+    dio_events_start_times = dio_times[dio_events_start_idx]
+    dio_events_end_times = dio_times[dio_events_end_idx]
+
+    # pad end times with end time of the epoch data to account for the case where the homelight was left on after the run session
+    len_diff = len(dio_events_start_times) - len(dio_events_end_times)
+    for i in range(len_diff):
+        dio_events_end_times = np.append(dio_events_end_times, epoch_end_time)
+
+    dio_events_intervals = np.vstack([dio_events_start_times, dio_events_end_times]).T
+
+    return dio_events_start_times, dio_events_end_times, dio_events_intervals
